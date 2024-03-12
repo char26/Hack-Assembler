ddf7b84466a8534009b7d738fdbd1396c62a516a
Charlie Alders <charliealders26@gmail.com>
2024-03-12 15:07:03 -0700

    more progress

--- src/AssemblyFileParser.java
+++ src/AssemblyFileParser.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class AssemblyFileParser {
     private List<String> cleanAssemblyCode;
     private Scanner fileReader;
+    private List<Instruction> parsedAssemblyInstructions;
 
     public AssemblyFileParser(String fileName) throws FileNotFoundException {
         cleanAssemblyCode = new ArrayList<>();
@@ -22,18 +23,6 @@ public class AssemblyFileParser {
         fileReader.close();
     }
 
-    public void makeFirstPass() {
-        String rawLine, cleanLine;
-        while (fileReader.hasNextLine()) {
-            rawLine = fileReader.nextLine();
-            cleanLine = clean(rawLine);
-            if (!cleanLine.isEmpty()) {
-                cleanAssemblyCode.add(cleanLine);
-            }
-        }
-
-    }
-
     public String clean(String rawLine) {
         String cleanLine = rawLine.replaceAll("\\s+", " ").trim();
         // I am replacing spaces with empty strings here to account for something like
@@ -45,6 +34,59 @@ public class AssemblyFileParser {
             cleanLine = cleanLine.substring(0, commentIndex);
         }
         return cleanLine;
+    }
+
+    public List<Instruction> getParsedAssemblyInstructions() {
+        return parsedAssemblyInstructions;
+    }
+
+    private void insertLabelInSymbolTable(String cleanLine, int address) throws Exception {
+        int firstParen = cleanLine.indexOf("(");
+        int secondParen = cleanLine.indexOf(")");
+        if (firstParen == -1 || secondParen == -1) {
+            throw new Exception("Labels need to be wrapped in parenthesis ().");
+        }
+        String labelName = cleanLine.substring(firstParen + 1, secondParen);
+        SymbolTable.add(labelName, address);
+    }
+
+    public void makeFirstPass() {
+        String rawLine, cleanLine;
+        while (fileReader.hasNextLine()) {
+            rawLine = fileReader.nextLine();
+            cleanLine = clean(rawLine);
+            if (!cleanLine.isEmpty()) {
+                cleanAssemblyCode.add(cleanLine);
+            }
+        }
+    }
+
+    public void makeSecondPass() {
+        String cleanLine;
+        int currentVariable = 16;
+        while (fileReader.hasNextLine()) {
+            cleanLine = fileReader.nextLine();
+            // A couple of sanity checks to make sure the lines are actually clean
+            assert (!cleanLine.isEmpty());
+            assert (cleanLine.indexOf("//") == -1);
+            // If we encounter a label
+            if (cleanLine.startsWith("(")) {
+                try {
+                    insertLabelInSymbolTable(cleanLine, currentVariable);
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
+                currentVariable++;
+            }
+
+            // Handle A-Instructions
+            // TODO: handle jumps using @LABEL
+            if (cleanLine.startsWith("@")) {
+                AInstruction aInst = new AInstruction(cleanLine);
+                parsedAssemblyInstructions.add(aInst);
+            }
+            // TODO: C-Instruction
+        }
     }
 
     public String toString() {
---
+++ src/CInstruction.java
@@ -0,0 +1,108 @@
+import java.util.HashMap;
+
+public class CInstruction {
+    private String comp;
+    private HashMap<String, String> compCodes;
+    private String dest;
+    private HashMap<String, String> destCodes;
+    private String jump;
+    private HashMap<String, String> jumpCodes;
+
+    public CInstruction(String code) {
+        if (code.startsWith("@")) {
+            throw new IllegalArgumentException("C instructions must not start with @");
+        }
+
+        compCodes = new HashMap<String, String>();
+        compCodes.put("0", "101010");
+        compCodes.put("1", "111111");
+        compCodes.put("-1", "111010");
+        compCodes.put("D", "001100");
+        compCodes.put("A", "110000");
+        compCodes.put("M", "110000");
+        compCodes.put("!D", "001101");
+        compCodes.put("!A", "110001");
+        compCodes.put("!M", "110001");
+        compCodes.put("-D", "001111");
+        compCodes.put("-A", "110011");
+        compCodes.put("-M", "110011");
+        compCodes.put("D+1", "011111");
+        compCodes.put("A+1", "110111");
+        compCodes.put("M+1", "110111");
+        compCodes.put("D+A", "000010");
+        compCodes.put("D+M", "000010");
+        compCodes.put("D-A", "010011");
+        compCodes.put("D-M", "010011");
+        compCodes.put("A-D", "000111");
+        compCodes.put("M-D", "000111");
+        compCodes.put("D&A", "000000");
+        compCodes.put("D&M", "000000");
+        compCodes.put("D|A", "010101");
+        compCodes.put("D|M", "010101");
+        destCodes = new HashMap<String, String>();
+        destCodes.put("M", "001");
+        destCodes.put("D", "010");
+        destCodes.put("MD", "011");
+        destCodes.put("A", "100");
+        destCodes.put("AM", "101");
+        destCodes.put("AD", "110");
+        destCodes.put("AMD", "111");
+        jumpCodes = new HashMap<String, String>();
+        jumpCodes.put("JGT", "001");
+        jumpCodes.put("JEQ", "010");
+        jumpCodes.put("JGE", "011");
+        jumpCodes.put("JLT", "100");
+        jumpCodes.put("JNE", "101");
+        jumpCodes.put("JLE", "110");
+        jumpCodes.put("JMP", "111");
+        try {
+            parseCodeIntoParts(code);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    private void parseCodeIntoParts(String code) throws Exception {
+        int eqIndex = code.indexOf("=");
+        if (eqIndex != -1) {
+            dest = code.substring(0, eqIndex);
+        }
+        int scIndex = code.indexOf(";");
+        if (scIndex != -1) {
+            jump = code.substring(scIndex + 1);
+        }
+        // There are 4 scenarios to consider to find the comp part
+        // 1) No destination and no jump (D)
+        if (eqIndex == -1 && scIndex == -1) {
+            comp = code;
+        }
+        // 2) No destination and a jump (0;JMP)
+        else if (eqIndex == -1 && scIndex != -1) {
+            comp = code.substring(0, scIndex);
+        }
+        // 3) A destination and no jump (D=M)
+        else if (eqIndex != -1 && scIndex == -1) {
+            comp = code.substring(eqIndex + 1);
+        }
+        // 4) A destination and a jump (D=M;JMP)
+        else if (eqIndex != -1 && scIndex != -1) {
+            comp = code.substring(eqIndex + 1, scIndex);
+        }
+        // Otherwise we have big problems
+        else {
+            throw new Exception("Something is wrong with comp parsing logic");
+        }
+    }
+
+    public String toString() {
+        StringBuilder assemblyBuilder = new StringBuilder();
+        if (dest != null) {
+            assemblyBuilder.append(dest + "=");
+        }
+        assemblyBuilder.append(comp);
+        if (jump != null) {
+            assemblyBuilder.append(";" + jump);
+        }
+        return assemblyBuilder.toString();
+    }
+}
