e4926aa1c1e96e3ca44a5c46d5e37cc9d5985f15
char26 <charliealders26@gmail.com>
2024-03-14 14:39:49 -0700

    working with gson

--- src/CInstruction.java
+++ src/CInstruction.java
@@ -1,5 +1,15 @@
 import java.util.HashMap;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CInstruction extends Instruction {
     private String comp;
     private HashMap<String, String> compCodes;
@@ -12,56 +22,21 @@ public class CInstruction extends Instruction {
         if (code.startsWith("@")) {
             throw new IllegalArgumentException("C instructions must not start with @");
         }
-        // TODO: use Gson or another library to import this data from a json file
-        compCodes = new HashMap<String, String>();
-        compCodes.put("0", "0101010");
-        compCodes.put("1", "0111111");
-        compCodes.put("-1", "0111010");
-        compCodes.put("D", "0001100");
-        compCodes.put("A", "0110000");
-        compCodes.put("M", "1110000");
-        compCodes.put("!D", "0001101");
-        compCodes.put("!A", "0110001");
-        compCodes.put("!M", "1110001");
-        compCodes.put("-D", "0001111");
-        compCodes.put("-A", "0110011");
-        compCodes.put("-M", "1110011");
-        compCodes.put("D+1", "0011111");
-        compCodes.put("A+1", "0110111");
-        compCodes.put("M+1", "1110111");
-        compCodes.put("D-1", "0001110");
-        compCodes.put("A-1", "0110010");
-        compCodes.put("M-1", "1110010");
-        compCodes.put("D+A", "0000010");
-        compCodes.put("D+M", "1000010");
-        compCodes.put("D-A", "0010011");
-        compCodes.put("D-M", "1010011");
-        compCodes.put("A-D", "0000111");
-        compCodes.put("M-D", "1000111");
-        compCodes.put("D&A", "0000000");
-        compCodes.put("D&M", "1000000");
-        compCodes.put("D|A", "0010101");
-        compCodes.put("D|M", "1010101");
+        Path path = Paths.get("../codes.json");
+        try {
+            String jsonString = Files.readString(path, StandardCharsets.UTF_8);
+            Gson gson = new Gson();
+            Type type = new TypeToken<HashMap<String, HashMap<String, String>>>() {
+            }.getType();
+            HashMap<String, HashMap<String, String>> map = gson.fromJson(jsonString, type);
+            compCodes = map.get("comp");
+            destCodes = map.get("dest");
+            jumpCodes = map.get("jump");
+            map = null;
+        } catch (IOException e) {
+            System.err.println(e);
+        }
 
-        destCodes = new HashMap<String, String>();
-        destCodes.put("null", "000");
-        destCodes.put("M", "001");
-        destCodes.put("D", "010");
-        destCodes.put("MD", "011");
-        destCodes.put("A", "100");
-        destCodes.put("AM", "101");
-        destCodes.put("AD", "110");
-        destCodes.put("AMD", "111");
-
-        jumpCodes = new HashMap<String, String>();
-        jumpCodes.put("null", "000");
-        jumpCodes.put("JGT", "001");
-        jumpCodes.put("JEQ", "010");
-        jumpCodes.put("JGE", "011");
-        jumpCodes.put("JLT", "100");
-        jumpCodes.put("JNE", "101");
-        jumpCodes.put("JLE", "110");
-        jumpCodes.put("JMP", "111");
         try {
             parseCodeIntoParts(code);
         } catch (Exception e) {
