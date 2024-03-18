c7221db98c24539242c83314e85847595fef3ac3
char26 <charliealders26@gmail.com>
2024-03-17 17:23:09 -0700

    added workflow

--- tests/compare-files-test.sh
+++ tests/compare-files-test.sh
@@ -1,1 +1,3 @@
-diff
+diff --ignore-all-space ./tests/compare/Rect.hack ./Rect-bin.hack
+diff --ignore-all-space ./tests/compare/Pong.hack ./Pong-bin.hack
+diff --ignore-all-space ./tests/compare/Max.hack ./Max-bin.hack
